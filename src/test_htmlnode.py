74e5582783f9b0e6d194495da99029fa54f11610
Drew Elliott <drew@beaverlab.dev>
2024-11-09 23:17:07 -0500

    added additional unit tests for htmlnode and fixed formatting error in props

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,30 +3,47 @@ import unittest
 
 from htmlnode import HTMLNode
 
-# Need to test these conditions: 
-#  - An HTMLNode without a tag will render as raw text
-#  - An HTMLNode without a value will be assumed to have children
-#  - An HTMLNode without children will be assumed to have a value
-#  - An HTMLNode without props simply won't have any attributes
 
 class TestHTMLNode(unittest.TestCase):
-    def test_tag(self):
-        # not sure how to test this one yet
-        pass
 
-    def test_value(self):
-        node = HTMLNode(tag='p', 
-                value='The quick brown fox jumped over the lazy dog', 
-                props={'href': 'https://www.google.com', 'target': '_blank'})
-        expected_props = ' href=https://www.google.com target=_blank'
-        self.assertEqual(node.props_to_html(), expected_props)
+    def test_initialization(self):
+        node = HTMLNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
+    def test_children(self):
+        node = HTMLNode(
+                tag='ul',
+                children='li'
+        )
+        self.assertEqual(node.children, "li")
+
+    def test_to_html(self):
+        node = HTMLNode()
+        self.assertRaises(NotImplementedError, node.to_html)
+        
+    def test_empty_props(self):
+        node = HTMLNode(tag='h1', value="Header1") 
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html(self):
+        node = HTMLNode(
+                tag='a',
+                props={"href": "https://www.yahoo.com"}
+        )
+        self.assertEqual(node.props_to_html(), ' href="https://www.yahoo.com"')
 
     def test_repr(self):
-        node = HTMLNode(tag='p', 
-                value='The quick brown fox jumped over the lazy dog', 
-                props={'href': 'https://www.google.com', 'target': '_blank'})
-        expected_repr = "HTMLNode(p, The quick brown fox jumped over the lazy dog, None, {'href': 'https://www.google.com', 'target': '_blank'})"
+        node = HTMLNode(
+                tag='p', 
+                value='The quick...', 
+                props={'target': '_blank'}
+        )
+        expected_repr = "HTMLNode(tag='p', value='The quick...', children=None, props={'target': '_blank'})"
         self.assertEqual(repr(node), expected_repr)
+
 
 if __name__ == "__main__":
     unittest.main()
