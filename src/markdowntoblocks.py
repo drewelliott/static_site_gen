44e6e7898c2ea54d1ab43bca73d3126e6e2f0df9
Elliott Drew <drew.elliott@nokia.com>
2024-11-19 17:25:13 -0700

    add markdown to blocks and unit tests

---
+++ src/markdowntoblocks.py
@@ -0,0 +1,11 @@
+
+
+def markdown_to_blocks(markdown):
+    block_list = []
+    md_list = markdown.split("\n\n")
+    for item in md_list:
+        if item.strip():
+            block_list.append(item.strip())
+
+    return block_list
+        
