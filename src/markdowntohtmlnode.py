6afe72032bafef8bb60969d79b0db91a3b3afb6c
Elliott Drew <drew.elliott@nokia.com>
2024-11-22 05:20:27 -0700

    starting md to html node

--- src/markdowntohtmlnode.py
+++ src/markdowntohtmlnode.py
@@ -3,11 +3,11 @@ from src.blocktoblocktype import block_to_block_type
 from src.htmlnode import HTMLNode, LeafNode, ParentNode
 
 def markdown_to_html_node(markdown: str) -> ParentNode:
-    # split the markdown into blocks
-    #
-    # loop over each block
-    # determine the type of block
-    # create new HTMLNode based on the type of block
+    md_blocks = markdown_to_blocks(markdown)
+
+    for block in md_blocks:
+        block_type = block_to_block_type(block)
+        block_node = LeafNode(block, block_type)
     # Assign the proper child HTMLNode objects to the block node
     # -- I created a shared text_to_children(text) function that 
     # works for all block types. It takes a string of text and returns 
