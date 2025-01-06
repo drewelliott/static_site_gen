600588ca0d9432d1178a65897899808ec10b4ce8
Elliott Drew <drew.elliott@nokia.com>
2025-01-06 10:48:26 -0500

    adding markdowntohtmlnode.py initial

--- src/markdowntohtmlnode.py
+++ src/markdowntohtmlnode.py
@@ -7,13 +7,15 @@ def markdown_to_html_node(markdown: str) -> ParentNode:
 
     for block in md_blocks:
         block_type = block_to_block_type(block)
-        block_node = LeafNode(block, block_type)
+        block_node = LeafNode(block_type, block)
     # Assign the proper child HTMLNode objects to the block node
-    # -- I created a shared text_to_children(text) function that 
-    # works for all block types. It takes a string of text and returns 
-    # a list of HTMLNodes that represent the inline markdown using 
-    # previously created functions (think TextNode -> HTMLNode)
-    # 
     html_node = ParentNode()
 
     return html_node
+
+    
+def text_to_children(text:str) -> List['HTMLNode']:
+    # text_to_children takes a string of text and returns 
+    # a list of HTMLNodes that represent the inline markdown using 
+    # previously created functions (think TextNode -> HTMLNode)
+    # 
