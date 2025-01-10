8c953c9b9df9a2181971ea52f73a76ddc97d543d
Drew Elliott <drew@beaverlab.dev>
2025-01-10 17:40:04 -0500

    adding static

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
-print("hello world")
+import shutil
+from pathlib import Path
+from typing import NoReturn
+
+
+def copy_static_to_public() -> NoReturn:
+
+    script_dir = Path(__file__).parent
+    static_dir = script_dir / "static"
+    public_dir = script_dir / "public"
+
+    if public_dir.exists():
+        shutil.rmtree(public_dir)
+
+    public_dir.mkdir(parents=True, exist_ok=True)
+
+    for item in static_dir.iterdir():
