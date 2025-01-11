b9adbe57bc68a61ac6708752d280581bc062804d
Drew Elliott <drew@beaverlab.dev>
2025-01-10 20:47:53 -0500

    updated main.py to copy static files to the public dir

--- src/main.py
+++ src/main.py
@@ -2,18 +2,43 @@
 
 import shutil
 from pathlib import Path
-from typing import NoReturn
 
 
-def copy_static_to_public() -> NoReturn:
+def copy_static_to_public() -> None:
 
     script_dir = Path(__file__).parent
-    static_dir = script_dir / "static"
-    public_dir = script_dir / "public"
+    static_dir = script_dir.parent / "static"
+    public_dir = script_dir.parent / "public"
 
+    print(f"Checking if {public_dir} exists...")
     if public_dir.exists():
+        print(f"Removing {public_dir}...")
         shutil.rmtree(public_dir)
 
+    print(f"Creating fresh {public_dir}...")
     public_dir.mkdir(parents=True, exist_ok=True)
 
-    for item in static_dir.iterdir():
+    copy_recursive(static_dir, public_dir)
+
+
+def copy_recursive(source: Path, dest: Path) -> None:
+    print(f"Source: {source}")
+    print(f"Dest: {dest}")
+    if source.is_file():
+        dest.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy(source, dest)
+
+    if source.is_dir():
+        dest.mkdir(exist_ok=True)
+        for item in source.iterdir():
+            relative_path = item.relative_to(source)
+            new_dest = dest / relative_path
+            copy_recursive(item, new_dest)
+
+
+def main() -> None:
+    copy_static_to_public()
+
+
+if __name__ == "__main__":
+    main()
